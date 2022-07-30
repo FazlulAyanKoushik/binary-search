3d1a54811bd170c055d750f9a5607aeaaae6c3e2
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2022-07-30 13:51:02 +0600

    issue fixed

--- binary-search.py
+++ binary-search.py
@@ -8,23 +8,26 @@ def binarySearch(list, target, left, right):
     count = count + 1
     if left <= right:
         mid = (left+right)//2
-        print(f'Middle number is {list[mid]} ')
+        print(f'Middle number is {list[mid]} : Left -> {left} right -> {right} middle -> {mid} ')
     
-    if target == list[mid]:
-        print(f'The Target number ({target}) is found is index number {mid} and loop count {count}')
-    
-    if target > list[mid]:
-        binarySearch(list, target, mid+1, right)
-    
-    if target < list[mid]:
-        binarySearch(list, target, left, mid-1)
+        if target == list[mid]:
+            print(f'The Target number ({target}) is found is index number {mid} and loop count {count}')
+            
+        if target > list[mid]:
+            binarySearch(list, target, mid+1, right)
         
-numbers = [2,12,14,6,45,3,4,9,23]
+        if target < list[mid]:
+            binarySearch(list, target, left, mid-1)
+    
+    else:
+        print('The number is not found in list')
+        
+numbers = [9,12,6,4,38,46,36,16,45,12,17,2,4,25,12,14,22,20]
 sorted_numbers = sorted(numbers)
 print(f'Sorted numbers are {sorted_numbers} ')
 left = 0
 right = len(sorted_numbers)-1
-target = 12
+target = 4
 binarySearch(sorted_numbers, target, left, right)
 
         
